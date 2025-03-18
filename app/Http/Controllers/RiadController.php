8e4ae730811188631d6912017ac6566237b1f685
Mohammed-Ben-Cheikh <bencheikh.official@gmail.com>
2025-03-18 13:37:13 +0000

    Add Riad model, controller, requests, policy, factory, migration, and seeder

---
+++ app/Http/Controllers/RiadController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Riad;
+use App\Http\Requests\StoreRiadRequest;
+use App\Http\Requests\UpdateRiadRequest;
+
+class RiadController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreRiadRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Riad $riad)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateRiadRequest $request, Riad $riad)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Riad $riad)
+    {
+        //
+    }
+}
